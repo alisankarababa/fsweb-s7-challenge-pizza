f35262cf138bae818c7e1dbc745f131326457f00
Alişan Karababa <alisankarababa@gmail.com>
2023-10-13 10:20:32 +0300

    update test

--- cypress/e2e/SiparişFormu.cy.js
+++ cypress/e2e/SiparişFormu.cy.js
@@ -53,35 +53,6 @@ describe('Sipariş Formu testleri', () => {
         }
     })
 
-    
-
-    function enterValidInput() {
-        const validInput = {
-            name: "Name1",
-            doughThickness: sampleProduct.availableDoughThicknesses[sampleProduct.availableDoughThicknesses.length - 1],
-            size: sampleProduct.availableSizes[2],
-            extraIngredient: sampleProduct.availableExtraIngredients[2]
-        }
-        cy.get(`[data-cy=${testAttributes.inputCustomerName}]`).type(validInput.name);
-        cy.get(`[data-cy=${validInput.size.replace(" ", "")}]`).check();
-        cy.get(`[data-cy=${testAttributes.inputDoughThickness}]`).select(validInput.doughThickness);
-    }
-
-    it("checks if entering valid data enables submit button and gives no error", () => {
-        enterValidInput();
-        cy.get(`[data-cy=${testAttributes.error}]`).should("not.exist");
-        cy.get(`[data-cy="submit"]`).should("be.enabled");
-    })
-
-    it.only("checks if submitting valid data redirects to success page", () => {
-
-        enterValidInput();
-
-        cy.get(`[data-cy="submit"]`).click();
-
-        cy.url().should("eq", urlPageSuccess);
-    })
-
     it("checks if dropdown errors show up", () => {
         cy.get(`[data-cy=${testAttributes.inputDoughThickness}]`).select(1);
         cy.get(`[data-cy=${testAttributes.inputDoughThickness}]`).select(0);
@@ -99,53 +70,41 @@ describe('Sipariş Formu testleri', () => {
         cy.get(`[data-cy=${testAttributes.error}]`).should("have.length", 1).should("have.text", errorMessages.errorNameInputMin);
     })
 
-    
 
 
+    function enterValidInput() {
+        const validInput = {
+            name: "Name1",
+            doughThickness: sampleProduct.availableDoughThicknesses[sampleProduct.availableDoughThicknesses.length - 1],
+            size: sampleProduct.availableSizes[2],
+            extraIngredient: sampleProduct.availableExtraIngredients[2]
+        }
+        cy.get(`[data-cy=${testAttributes.inputCustomerName}]`).type(validInput.name);
+        cy.get(`[data-cy=${validInput.size.replace(" ", "")}]`).check();
+        cy.get(`[data-cy=${testAttributes.inputDoughThickness}]`).select(validInput.doughThickness);
 
-    
+        cy.get(`[data-cy=${sampleProduct.availableExtraIngredients[0].replace(" ", "")}]`).check();
+        cy.get(`[data-cy=${sampleProduct.availableExtraIngredients[1].replace(" ", "")}]`).check();
+        cy.get(`[data-cy=${sampleProduct.availableExtraIngredients[2].replace(" ", "")}]`).check();
 
+    }
 
+    it("checks if entering valid data enables submit button and gives no error", () => {
+        enterValidInput();
+        cy.get(`[data-cy=${testAttributes.error}]`).should("not.exist");
+        cy.get(`[data-cy="submit"]`).should("be.enabled");
+    })
+
+    it("checks if submitting valid data redirects to success page", () => {
+
+        enterValidInput();
+
+        cy.get(`[data-cy="submit"]`).click();
+
+        cy.url().should("eq", urlPageSuccess);
+    })
 })
 
-    //Kırmızı bir header kısmı
-    //Yukarıda bir logo olmalı
-    //altında Anasayfa-Seçenekler-Sipariş Oluştur yazısı p tagi ile yap Sipariş oluştur bold olsun
-
-
-    //Ürün ismi bu durumda Position Absolute Acı Pizza bold
-    //85.50tl 4.9 (200)
-
-    // Açıklama 
-    // Frontend Dev olarak hala position absolute kullanıyorsan bu çok acı pizza
-    // tam sana göre. Pizza, domates, peynir ve genellikle çeşitli diğer malzemelerle kaplanmış, daha sonra
-    // geneleneksel olarak odun atelinde bir fırında yüksek sıcaklıkta pişirilen, genellikle yuvarlak, düzleştirilmiş
-    // mayalı buğday bazlı hamurdan oluşan İtlyan kökenli lezzetl bir yemektir.. Küçük bir pizzaya bazen 
-    // pizzetta denir.
-
-
-    //solda "Boyut Seç" kırmızı yıldız var yanında
-    //altında arrSizes = [Küçük, Orta, Büyük] radio button
-
-    //sağda "Hamur Seç" kırmızı yıldız var yanında
-    //altında arrDoughThickness = ["Hamur Kalınlığı", "Kalın", "İnce"]
-
-    //bu ikisi altında "Ek Malzemeler"
-    //p tagi ile "En Fazla 10 malzeme seçebilirsiniz. 5₺"
-    // check box ile ek malzemeler 
-    // arrExtraIngredients = ["Pepperoni", "Domates", "Biber", "Sosis", "Mısır", "Sucuk", "Kanada Jambonu", "Ananas", "Jalepeno", "Soğan", "Sarımsak" ]
-
-    //Sipariş Notu bold
-    //input text isim için
-    //input text placeholder "Siparişine eklemek istediğin bir not var mı?"
-    // horizontal rule 
-
-    // solda - butonu, adet, + butonu
-    //sağda bir kart içeriği yukarıdan aşağıya doğru
-    // "Sipariş Toplamı" bold
-    // "Seçimler"   ekstra malzeme fiyat toplamı
-    // "Toplam" ekstra seçimler artı pizza fiyatı 
-    // Büyük "SİPARİŞ VER" butonu
 
 
 
